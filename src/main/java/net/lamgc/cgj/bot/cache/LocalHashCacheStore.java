ac38e1b5cf3337c9e8a563d0ae776f443f6753df
LamGC <lam827@lamgc.net>
2020-04-16 23:48:59 +0800

    [Change] CacheObject, LocalHashCacheStore 合并类;

--- src/main/java/net/lamgc/cgj/bot/cache/LocalHashCacheStore.java
+++ src/main/java/net/lamgc/cgj/bot/cache/LocalHashCacheStore.java
@@ -1,8 +1,11 @@
 package net.lamgc.cgj.bot.cache;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.Date;
 import java.util.Hashtable;
 import java.util.Objects;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class LocalHashCacheStore<T> implements CacheStore<T> {
 
@@ -99,4 +102,43 @@ public class LocalHashCacheStore<T> implements CacheStore<T> {
     public boolean supportedList() {
         return false;
     }
+
+
+    public static class CacheObject<T> implements Comparable<CacheObject<T>> {
+
+        private AtomicReference<T> value;
+        private AtomicReference<Date> expire;
+
+        public CacheObject(T value, Date expire) {
+            this.value = new AtomicReference<>(value);
+            this.expire = new AtomicReference<>(expire);
+        }
+
+        public synchronized void update(T value, Date newExpire) {
+            if(new Date().after(newExpire)) {
+                throw new IllegalArgumentException("Due earlier than current time");
+            }
+            this.expire.set(newExpire);
+            this.value.set(value);
+        }
+
+        public synchronized T get() {
+            return value.get();
+        }
+
+        public Date getExpireDate() {
+            return expire.get();
+        }
+
+        public boolean isExpire(Date time) {
+            Date expireDate = getExpireDate();
+            return expireDate != null && expireDate.before(time);
+        }
+
+        @Override
+        public int compareTo(@NotNull CacheObject<T> o) {
+            return this.getExpireDate().after(o.getExpireDate()) ? -1 : 1;
+        }
+    }
+
 }
