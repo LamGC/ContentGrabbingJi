cbd10ff2810a43e71d96a9b4fc15b2d344069423
LamGC <lam827@lamgc.net>
2020-05-07 18:24:09 +0800

    [Fix] Issue #4 修复因Mirai API变动导致应用无法从消息获取ImageId的问题;

--- src/main/java/net/lamgc/cgj/bot/framework/mirai/message/MiraiMessageEvent.java
+++ src/main/java/net/lamgc/cgj/bot/framework/mirai/message/MiraiMessageEvent.java
@@ -16,7 +16,7 @@ public class MiraiMessageEvent extends MessageEvent {
 
     public MiraiMessageEvent(ContactMessage message) {
         super(message instanceof GroupMessage ? ((GroupMessage) message).getGroup().getId() : 0,
-                message.getSender().getId(), message.getMessage().contentToString());
+                message.getSender().getId(), getMessageBodyWithoutSource(message.getMessage().toString()));
         this.messageObject = Objects.requireNonNull(message);
         if(message instanceof GroupMessage) {
             messageSender = new MiraiMessageSender(((GroupMessage) message).getGroup(), MessageSource.Group);
@@ -24,6 +24,19 @@ public class MiraiMessageEvent extends MessageEvent {
             messageSender = new MiraiMessageSender(message.getSender(), MessageSource.Private);
         }
 
+    }
+
+    /**
+     * 将ContactMessage获得的消息内容删除 Mirai:source 并返回.
+     * <p>该做法比较保守, 防止Mirai:source位置出现变动.</p>
+     * @param message ContactMessage的消息内容;
+     * @return 返回删除了Mirai:source的消息
+     */
+    private static String getMessageBodyWithoutSource(String message) {
+        StringBuilder builder = new StringBuilder(message);
+        int startIndex = builder.indexOf("[mirai:source:");
+        int endIndex = builder.indexOf("]", startIndex) + 1;
+        return builder.delete(startIndex, endIndex).toString();
     }
 
     @Override
