fe8bf16d3af14cd583845c959ca93b5459cdc6ea
LamGC <lam827@lamgc.net>
2020-05-25 20:23:14 +0800

    [Clean] RandomRankingArtworksSender 清理无用导入; [Change] AutoCleanTimer 统一LoggerName名称;

--- src/main/java/net/lamgc/cgj/bot/RandomRankingArtworksSender.java
+++ src/main/java/net/lamgc/cgj/bot/RandomRankingArtworksSender.java
@@ -7,7 +7,6 @@ import net.lamgc.cgj.pixiv.PixivURL;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
--- src/main/java/net/lamgc/cgj/bot/cache/AutoCleanTimer.java
+++ src/main/java/net/lamgc/cgj/bot/cache/AutoCleanTimer.java
@@ -15,7 +15,7 @@ public class AutoCleanTimer extends TimerTask {
 
     private final static Timer cleanTimer = new Timer("Thread-AutoClean", true);
 
-    private final static Logger log = LoggerFactory.getLogger(AutoCleanTimer.class.getName());
+    private final static Logger log = LoggerFactory.getLogger(AutoCleanTimer.class);
 
     static {
         cleanTimer.schedule(new AutoCleanTimer(), 100L);
