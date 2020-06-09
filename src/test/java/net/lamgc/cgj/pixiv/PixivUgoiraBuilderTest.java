18a8ad95a13b6a71074695c713e547ce4274f441
LamGC <lam827@lamgc.net>
2020-06-09 10:07:54 +0800

    [Change] PixivUgoiraBuilderTest 调整测试细节;

--- src/test/java/net/lamgc/cgj/pixiv/PixivUgoiraBuilderTest.java
+++ src/test/java/net/lamgc/cgj/pixiv/PixivUgoiraBuilderTest.java
@@ -1,7 +1,7 @@
 package net.lamgc.cgj.pixiv;
 
 import org.apache.http.HttpHost;
-import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.client.HttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.bouncycastle.util.io.Streams;
 import org.junit.Ignore;
@@ -19,8 +19,8 @@ public class PixivUgoiraBuilderTest {
     @Test
     public void buildTest() throws IOException {
         File outputFile = new File("./output2.gif");
-        CloseableHttpClient httpClient = HttpClientBuilder.create().setProxy(new HttpHost("127.0.0.1", 1001)).build();
-        PixivUgoiraBuilder builder = new PixivUgoiraBuilder(httpClient, 80766493);
+        HttpClient httpClient = HttpClientBuilder.create().setProxy(new HttpHost("127.0.0.1", 1080)).build();
+        PixivUgoiraBuilder builder = new PixivUgoiraBuilder(httpClient, 81163967);
         LoggerFactory.getLogger(PixivUgoiraBuilderTest.class).info("UgoiraMeta: {}", builder.getUgoiraMeta());
         InputStream inputStream = builder.buildUgoira(true);
         Files.write(outputFile.toPath(), Streams.readAll(inputStream));
