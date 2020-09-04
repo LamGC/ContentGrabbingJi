455f922cca1fefbdebee5c91d2e6add6742b9e84
LamGC <lam827@lamgc.net>
2020-09-04 23:27:37 +0800

    [Document] 更新 SetCacheStore 的一些说明;
    [Document] SetCacheStore 更新说明;

--- ContentGrabbingJi-CacheStore-api/src/main/java/net/lamgc/cgj/bot/cache/SetCacheStore.java
+++ ContentGrabbingJi-CacheStore-api/src/main/java/net/lamgc/cgj/bot/cache/SetCacheStore.java
@@ -26,8 +26,12 @@ import java.util.Set;
  * @author LamGC
  */
 public interface SetCacheStore<E> extends CollectionCacheStore<E, Set<E>> {
-
-
-
-
+    /*
+     * 说实话, SetCacheStore 的存在有点...奇怪, 或者可能就没有用,
+     * 因为根据 2 代对缓存存储的使用情况来看, 用到 Set 的地方根本就没有,
+     * 而且可能在某些方面实现起来也不是件容易事情.
+     * 所以 SetCacheStore 可能会废弃掉(最晚也会在正式版出来前),
+     * 或者有用的话才会为其设计相关方法.
+     * (比如偏向 Redis 的那套进行设计, 但是我希望不要出现偏向性, 为所有实现提供一个平衡的实现复杂度)
+     */
 }
